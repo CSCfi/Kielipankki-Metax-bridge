bf7a2748bf7d775b5873bfa31f1acef8d1113129
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-06-02 17:17:01 +0300

    create pmh_api class that returns all metadata records from given url

---
+++ harvester/pmh_interface.py
@@ -0,0 +1,30 @@
+"""
+Fetch data from an OAI-PMH API of Metashare
+"""
+
+from sickle import Sickle
+import requests
+
+
+class PMH_API:
+    """
+    Interface for fetching data from an OAI-PMH API
+    """
+
+    def __init__(self, url):
+        """
+        :param url: URL of the OAI-PMH API used
+        """
+        self.sickle = Sickle(url)
+
+    def get_all_metadata_records(self):
+        """
+        Get content for all records available from the API.
+        """
+        records = self.sickle.ListRecords(metadataPrefix='info')
+        metadata_records_list = []
+        for record in records:
+            metadata_record = record.raw
+            metadata_records_list.append(metadata_record)
+        return metadata_records_list
+
