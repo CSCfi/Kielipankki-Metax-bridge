0fafcee25f368a51ceda0ee74b5ccda0f1128369
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-06-28 16:13:23 +0300

    create class with properties

---
+++ harvester/cmdi_parser.py
@@ -0,0 +1,16 @@
+import json
+from lxml import etree
+
+
+class MSRecordParser:
+    def __init__(self, ms_record):
+        """
+        Create a Metashare record object.
+
+        :param ms_record: a Metashare record
+        """
+        ms_xml_string = etree.tostring(ms_record.xml)
+        self.ms_record_tree = etree.fromstring(ms_xml_string)
+
+
+
