2e91ffa208e271885b7dbf11046145c5c0209de1
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-07-25 14:35:40 +0300

    remove actor and organization related code

--- harvester/cmdi_parser.py
+++ harvester/cmdi_parser.py
@@ -55,70 +55,17 @@ class MSRecordParser:
 
         return result[0] if result else None
     
-    def get_organization_info(self, org_name):
-        koodistot_api = "https://koodistot.suomi.fi/codelist-api/api/v1/coderegistries/fairdata/codeschemes/organization/codes"
-        
-        try:
-            response = requests.get(url = koodistot_api, params = {"prefLabel": org_name})
-            data = response.json()
-
-            if data["results"]:
-                result = {}
-                result["code"] = data["results"][0]["codeValue"]
-                result["in_scheme"] = data["results"][0]["codeScheme"]["uri"]
-                result["pref_label"] = data["results"][0]["prefLabel"]
-                return result
-            else: #the API returns 200 even when no appropriate organization is returned
-                raise ValueError("No results found")
-        except ValueError:
-            return ""
-
-
-    def get_metadata_creators(self):
-        metadata_creators = self.ms_record_tree.xpath("//info:metadataCreator", namespaces={'info': 'http://www.ilsp.gr/META-XMLSchema'})
-
-        actors = []
-        for metadata_creator in metadata_creators:
-            first_name = self.get_value(metadata_creator, "info:givenName")
-            surname = self.get_value(metadata_creator, "info:surname")
-            org_name = self.get_value(metadata_creator, "info:affiliation/info:organizationName")
-            if self.get_organization_info(org_name) == "":
-                code = ""
-                in_scheme = ""
-                pref_label = self.get_language_contents(metadata_creator, "info:affiliation/info:organizationName")
-            else:
-                code = self.get_organization_info(org_name)["code"]
-                in_scheme = self.get_organization_info(org_name)["in_scheme"]
-                pref_label = self.get_organization_info(org_name)["pref_label"]
-
-            actor = {
-                "person": f"{first_name} {surname}",
-                "organization": {
-                    "code": code,
-                    "in_scheme": in_scheme,
-                    "pref_label": pref_label
-                }
-            }
-
-            actors.append({
-                "role": "creator",
-                "actor": actor
-            })
-
-        return {"actors": actors}
 
     def json_converter(self):
         identifier = self.get_identifier()
         description = self.get_language_contents(self.ms_record_tree, "//info:description")
-        metadata_creators = self.get_metadata_creators()
         title = self.get_language_contents(self.ms_record_tree, '//info:resourceName')
 
 
         output = {
             "persistent_identifier": identifier[0],
             "title": title,
-            "description": description,
-            "actors": metadata_creators["actors"]
+            "description": description
         }
 
         return json.dumps(output)
