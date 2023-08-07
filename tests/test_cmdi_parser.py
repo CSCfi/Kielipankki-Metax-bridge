288a7d83e26c294e00a4d46972db2e765e315860
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-08-07 10:57:13 +0300

    change date related tests for correct format

--- tests/test_cmdi_parser.py
+++ tests/test_cmdi_parser.py
@@ -30,16 +30,16 @@ def test_get_identifier(single_record):
     expected_result = "urn.fi/urn:nbn:fi:lb-2017021609"
     assert result == expected_result
 
-def test_get_modified(single_record):
+def test_get_modified_date(single_record):
     record = MSRecordParser(single_record)
-    result = record._get_text_xpath("//cmd:metadataInfo/cmd:metadataLastDateUpdated/text()")
-    expected_result = "2017-02-15"
+    result = record._get_date("//cmd:metadataInfo/cmd:metadataLastDateUpdated/text()")
+    expected_result = "2017-02-15T00:00:00.000000Z"
     assert result == expected_result
 
-def test_get_issued(single_record):
+def test_get_issued_date(single_record):
     record = MSRecordParser(single_record)
-    result = record._get_text_xpath("//cmd:metadataInfo/cmd:metadataCreationDate/text()")
-    expected_result = "2017-02-15"
+    result = record._get_date("//cmd:metadataInfo/cmd:metadataCreationDate/text()")
+    expected_result = "2017-02-15T00:00:00.000000Z"
     assert result == expected_result
  
 def test_get_title_not_empty(single_record):
@@ -59,16 +59,16 @@ def test_get_identifier_not_empty(single_record):
 
 def test_get_modified_not_empty(single_record):
     record = MSRecordParser(single_record)
-    result = record._get_text_xpath("//cmd:metadataInfo/cmd:metadataLastDateUpdated/text()")
+    result = record._get_date("//cmd:metadataInfo/cmd:metadataLastDateUpdated/text()")
     assert result != ""
 
 def test_get_issued_not_empty(single_record):
     record = MSRecordParser(single_record)
-    result = record._get_text_xpath("//cmd:metadataInfo/cmd:metadataCreationDate/text()")
+    result = record._get_date("//cmd:metadataInfo/cmd:metadataCreationDate/text()")
     assert result != ""
 
 def test_json_converter(single_record):
     record = MSRecordParser(single_record)
     result = record.json_converter()
-    expected_result = json.dumps({"persistent_identifier": "urn.fi/urn:nbn:fi:lb-2017021609", "title": {"en": "Silva Kiuru's Time Expressions Corpus", "fi": "Silva Kiurun ajanilmausaineisto"}, "description": {"en": "This corpus of time expressions has been compiled from literary works, translations, dialect texts as well as other texts. Format: word documents.", "fi": "T\u00e4m\u00e4 suomen kielen ajanilmauksia k\u00e4sitt\u00e4v\u00e4 aineisto on koottu kaunokirjallisten alkuper\u00e4isteosten, k\u00e4\u00e4nn\u00f6sten, murreaineistojen ja muiden tekstien pohjalta."}, "modified": "2017-02-15", "issued": "2017-02-15"})
+    expected_result = json.dumps({"persistent_identifier": "urn.fi/urn:nbn:fi:lb-2017021609", "title": {"en": "Silva Kiuru's Time Expressions Corpus", "fi": "Silva Kiurun ajanilmausaineisto"}, "description": {"en": "This corpus of time expressions has been compiled from literary works, translations, dialect texts as well as other texts. Format: word documents.", "fi": "T\u00e4m\u00e4 suomen kielen ajanilmauksia k\u00e4sitt\u00e4v\u00e4 aineisto on koottu kaunokirjallisten alkuper\u00e4isteosten, k\u00e4\u00e4nn\u00f6sten, murreaineistojen ja muiden tekstien pohjalta."}, "modified": "2017-02-15T00:00:00.000000Z", "issued": "2017-02-15T00:00:00.000000Z"})
     assert result == expected_result
