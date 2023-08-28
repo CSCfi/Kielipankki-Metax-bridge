b9773386b97272f88c7660ab994c2be565e123d9
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-08-28 15:32:24 +0300

    KP-7440 Make post request to Metax for new dataset

--- metax_api.py
+++ metax_api.py
@@ -1,4 +1,7 @@
 import requests
+from requests import HTTPError
+import json
+
 
 metax_base_url = "https://metax-service.fd-staging.csc.fi/v3"
 headers = {'Content-Type': 'application/json'}
@@ -13,3 +16,11 @@ def get_dataset_record_metax_id(dataset_pid):
     r = requests.get(f"{metax_base_url}/datasets?data_catalog{kielipankki_catalog_id}&persistent_identifier={dataset_pid}", headers=headers)
     if r.json()["count"] == 1:
         return r.json()["results"][0]["id"]
+
+def create_dataset(metadata_json):
+    r = requests.post(f"{metax_base_url}/datasets", json=metadata_json, headers=headers)
+    try:
+        r.raise_for_status()
+    except HTTPError as e:
+        raise
+    return json.loads(r.text)['id']
