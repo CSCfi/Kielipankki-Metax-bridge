f742ed2c299128abf69239b2abbb49e794ffb59d
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-08-28 15:30:23 +0300

    KP-7440 Get id of dataset from Metax

--- metax_api.py
+++ metax_api.py
@@ -8,3 +8,8 @@ def check_if_dataset_record_in_datacatalog(dataset_pid):
     r = requests.get(f"{metax_base_url}/datasets?data_catalog{kielipankki_catalog_id}&persistent_identifier={dataset_pid}", headers=headers)
     if r.json()["count"] == 1:
         return True
+
+def get_dataset_record_metax_id(dataset_pid):
+    r = requests.get(f"{metax_base_url}/datasets?data_catalog{kielipankki_catalog_id}&persistent_identifier={dataset_pid}", headers=headers)
+    if r.json()["count"] == 1:
+        return r.json()["results"][0]["id"]
