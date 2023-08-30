4c76e38b6a6e8951cb97d6d9e728e61516d49ec4
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-08-30 16:30:22 +0300

    KP-7440 Change variable names to be more accurate

--- metadata_harvester_cli.py
+++ metadata_harvester_cli.py
@@ -7,23 +7,22 @@ from harvester.metadata_parser import MSRecordParser
 import metax_api
 from lxml import etree
 
-
 def retrieve_metadata_content(url="https://kielipankki.fi/md_api/que"):
     """
     Fetch metadata records and transform them to JSON.
     """
     api = PMH_API(url)
-    mapped_data_dict = {}
+    all_mapped_data_dict = {}
     metadata_contents = api.get_all_metadata_records()
     for metadata_content in metadata_contents:
         lxml_record = etree.fromstring(etree.tostring(metadata_content.xml))
         metadata_record = MSRecordParser(lxml_record)
         if metadata_record.check_pid_exists():
             if metadata_record.check_resourcetype_corpus():
-                urn = metadata_record.get_identifier("//info:identificationInfo/info:identifier/text()")
-                mapped_data_dict[urn] = metadata_record.json_converter()
-    return mapped_data_dict
-
+                pid = metadata_record.get_identifier("//info:identificationInfo/info:identifier/text()")
+                all_mapped_data_dict[pid] = metadata_record.json_converter()
+    return all_mapped_data_dict
 
 if __name__ == "__main__":
     retrieve_metadata_content()
+        
