ea9a16ac8016c5c04c59266d7a4cb0241336ef2f
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-08-31 15:47:10 +0300

    KP-7440 Change return statement to return false also

--- metax_api.py
+++ metax_api.py
@@ -15,8 +15,7 @@ def check_if_dataset_record_in_datacatalog(dataset_pid):
     Check if a dataset with given PID exists in given data catalog.
     """
     r = requests.get(f"{metax_base_url}/datasets?data_catalog_id={kielipankki_catalog_id}&persistent_identifier={dataset_pid}", headers=headers)
-    if r.json()["count"] == 1: #Once Metax implements unique PIDs this check can be removed
-        return True
+    return r.json()["count"] == 1 #Once Metax implements unique PIDs this check can be removed
 
 def get_dataset_record_metax_id(dataset_pid):
     """
