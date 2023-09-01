24b148a1a5749d441107a0c7a793a23a1258f51c
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-09-01 19:38:05 +0300

    KP-7440 Update doctrings with params and returns

--- metax_api.py
+++ metax_api.py
@@ -18,6 +18,8 @@ kielipankki_catalog_id = "urn:nbn:fi:att:data-catalog-kielipankki-v3"
 def check_if_dataset_record_in_datacatalog(dataset_pid):
     """
     Check if a dataset with given PID exists in given data catalog.
+    :param dataset_pid: the persistent identifier of the resource
+    :return: boolean
     """
     r = requests.get(f"{metax_base_url}/datasets?data_catalog_id={kielipankki_catalog_id}&persistent_identifier={dataset_pid}", headers=headers)
     return r.json()["count"] == 1 #Once Metax implements unique PIDs this check can be removed
@@ -25,6 +27,8 @@ def check_if_dataset_record_in_datacatalog(dataset_pid):
 def get_dataset_record_metax_id(dataset_pid):
     """
     Get the UUID of a dataset from Metax.
+    :param dataset pid: the persistent identifier of the resource
+    :return: the dataset identifier in Metax
     """
     r = requests.get(f"{metax_base_url}/datasets?data_catalog_id={kielipankki_catalog_id}&persistent_identifier={dataset_pid}", headers=headers)
     if r.json()["count"] == 1: #Once Metax implements unique PIDs this check can be removed
@@ -33,6 +37,8 @@ def get_dataset_record_metax_id(dataset_pid):
 def create_dataset(metadata_dict):
     """
     Create a dataset record to Metax.
+    :param metadata_dict: dictionary of metadata mappings
+    :return: the dataset identifier in Metax
     """
     r = requests.post(f"{metax_base_url}/datasets", json=metadata_dict, headers=headers)
     try:
@@ -46,6 +52,8 @@ def create_dataset(metadata_dict):
 def update_dataset(metax_dataset_id, metadata_dict):
     """
     Update existing dataset record in Metax.
+    :param metadata_dict: dictionary of metadata mappings
+    :return: the dataset identifier in Metax
     """
     r = requests.put(f"{metax_base_url}/datasets/{metax_dataset_id}", json=metadata_dict, headers=headers)
     try:
