d39e11cdb869f4e85fe59c8eb29575cc3626c43b
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-09-11 15:32:43 +0300

    KP-7440 Add doctrings to fixtures and functions

--- tests/test_metadata_parser.py
+++ tests/test_metadata_parser.py
@@ -1,73 +1,80 @@
 import pytest
 from lxml import etree
-import json
 from harvester.metadata_parser import MSRecordParser
 
-
 def _get_file_as_lxml(filename):
+    """Returns given file as an lxml element."""
     with open(filename) as infile:
         return etree.fromstring(infile.read())
 
 @pytest.fixture
 def single_record():
+    """Well-formed record sample of Kielipankki metadata."""
     return _get_file_as_lxml("tests/test_data/kielipankki_record_sample.xml")
 
 def test_get_title(single_record):
+    """Testing that different language versions of "title" are mapped."""
     record = MSRecordParser(single_record)
     result = record._get_language_contents("//info:resourceName")
     expected_result = {"en": "Silva Kiuru's Time Expressions Corpus", "fi": "Silva Kiurun ajanilmausaineisto"}
     assert result == expected_result
 
 def test_get_description(single_record):
+    """Testing that different language versions of "description" are mapped."""
     record = MSRecordParser(single_record)
     result = record._get_language_contents("//info:description")
     expected_result = {"en": "This corpus of time expressions has been compiled from literary works, translations, dialect texts as well as other texts. Format: word documents.", "fi": "T\u00e4m\u00e4 suomen kielen ajanilmauksia k\u00e4sitt\u00e4v\u00e4 aineisto on koottu kaunokirjallisten alkuper\u00e4isteosten, k\u00e4\u00e4nn\u00f6sten, murreaineistojen ja muiden tekstien pohjalta."}
     assert result == expected_result
 
 def test_get_identifier(single_record):
+    """Check that a PID is returned."""
     record = MSRecordParser(single_record)
     result = record.get_identifier("//info:identificationInfo/info:identifier/text()")
     expected_result = "urn.fi/urn:nbn:fi:lb-2017021609"
     assert result == expected_result
 
 def test_get_modified_date(single_record):
+    """Check that the modified date is returned in correct format."""
     record = MSRecordParser(single_record)
     result = record._get_date("//info:metadataInfo/info:metadataLastDateUpdated/text()")
     expected_result = "2017-02-15T00:00:00.000000Z"
     assert result == expected_result
 
 def test_get_issued_date(single_record):
+    """Check that the issued date is returned in correct format."""
     record = MSRecordParser(single_record)
     result = record._get_date("//info:metadataInfo/info:metadataCreationDate/text()")
     expected_result = "2017-02-15T00:00:00.000000Z"
     assert result == expected_result
  
 def test_get_title_not_empty(single_record):
+    """Check that """
     record = MSRecordParser(single_record)
     result = record._get_language_contents("//info:resourceName")
-    assert result != {}
+    assert result
 
 def test_get_description_not_empty(single_record):
     record = MSRecordParser(single_record)
     result = record._get_language_contents("//info:description")
-    assert result != {}
+    assert result
 
 def test_get_identifier_not_empty(single_record):
     record = MSRecordParser(single_record)
     result = record.get_identifier("//info:identificationInfo/info:identifier/text()")
-    assert result != ""
+    assert result
 
 def test_get_modified_not_empty(single_record):
     record = MSRecordParser(single_record)
     result = record._get_date("//info:metadataInfo/info:metadataLastDateUpdated/text()")
-    assert result != ""
+    assert result
 
 def test_get_issued_not_empty(single_record):
     record = MSRecordParser(single_record)
     result = record._get_date("//info:metadataInfo/info:metadataCreationDate/text()")
-    assert result != ""
+    assert result
 
 def test_to_dict(single_record):
+    """Test that all relevant elements are mapped to a dictionary."""
     record = MSRecordParser(single_record)
     result = record.to_dict()
     expected_result = {
@@ -108,19 +115,22 @@ def test_to_dict(single_record):
 
 @pytest.fixture
 def tool_record():
+    """A record where resourceType is "toolService", not "corpus"."""
     return _get_file_as_lxml("tests/test_data/tool_record.xml")
 
 def test_check_resourcetype_corpus(tool_record):
+    """Check that a "toolService" record is spotted."""
     tool_record = MSRecordParser(tool_record)
     result = tool_record.check_resourcetype_corpus()
-    expected_result = None
-    assert result == expected_result
+    assert result is None
 
 @pytest.fixture
 def missing_license_record():
+    """A record missing licenseInfo element."""
     return _get_file_as_lxml("tests/test_data/missing_licenseinfo.xml")
 
 def test_missing_license_record(missing_license_record):
+    """Test that records missing licenseInfo element is mapped as expected."""
     missing_license_record = MSRecordParser(missing_license_record)
     result = missing_license_record._map_access_rights()
     expected_result = {
@@ -137,9 +147,11 @@ def test_missing_license_record(missing_license_record):
 
 @pytest.fixture
 def license_with_custom_url_record():
+    """A record containing lisence url in documentation elements."""
     return _get_file_as_lxml("tests/test_data/res_with_license_url.xml")
 
 def test_license_custom_url_record(license_with_custom_url_record):
+    """Test that license details and availability are mapped."""
     license_with_custom_url_record = MSRecordParser(license_with_custom_url_record)
     result = license_with_custom_url_record._map_access_rights()
     expected_result = {
@@ -157,9 +169,11 @@ def test_license_custom_url_record(license_with_custom_url_record):
 
 @pytest.fixture
 def several_licenses_record():
+    """A record that has several licenseInfo elements."""
     return _get_file_as_lxml("tests/test_data/several_licenses.xml")
 
 def test_several_licenses_record(several_licenses_record):
+    """Check that all licenses of a record are parsed and mapped."""
     several_licenses_record = MSRecordParser(several_licenses_record)
     result = several_licenses_record._map_access_rights()
     expected_result = {
@@ -181,9 +195,11 @@ def test_several_licenses_record(several_licenses_record):
 
 @pytest.fixture
 def accesstype_open_record():
+    """A record with a PUB license."""
     return _get_file_as_lxml("tests/test_data/accesstype_open.xml")
 
 def test_accesstype_open_record(accesstype_open_record):
+    """Check that records with open access are parsed and mapped."""
     accesstype_open_record = MSRecordParser(accesstype_open_record)
     result = accesstype_open_record._map_access_rights()
     expected_result = {
@@ -201,10 +217,10 @@ def test_accesstype_open_record(accesstype_open_record):
 
 @pytest.fixture
 def missing_pid_record():
+    """A record that doesn't have a PID."""
     return _get_file_as_lxml("tests/test_data/missing_pid.xml")
 
 def test_check_pid_exists(missing_pid_record):
+    """Check that a missing PID is handled."""
     missing_pid_record = MSRecordParser(missing_pid_record)
-    result = missing_pid_record.check_pid_exists()
-    expected_result = False
-    assert result == expected_result
+    assert not missing_pid_record.check_pid_exists()
