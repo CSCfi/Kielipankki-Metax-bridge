9e4795138ae4ab261d0636a4a45d34c4c6c09ad4
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-09-12 09:56:44 +0300

    KP-7440 Rename sample

--- tests/test_metadata_harvester_cli.py
+++ tests/test_metadata_harvester_cli.py
@@ -23,7 +23,7 @@ def single_record_xml():
     return _get_file_as_string("tests/test_data/kielipankki_record_sample.xml")
 
 @pytest.fixture
-def single_record_response(kielipankki_api_url, single_record_xml):
+def single_record_to_dict(kielipankki_api_url, single_record_xml):
     """
     A GET request that returns the XML data as a dictionary
     """
@@ -31,19 +31,19 @@ def single_record_response(kielipankki_api_url, single_record_xml):
         mocker.get(kielipankki_api_url, text=single_record_xml)
         yield {"urn.fi/urn:nbn:fi:lb-2017021609": {"data_catalog": "urn:nbn:fi:att:data-catalog-kielipankki-v3", "language": [{"url": "http://lexvo.org/id/iso639-3/fin"}], "field_of_science": [{"url": "http://www.yso.fi/onto/okm-tieteenala/ta112"}], "persistent_identifier": "urn.fi/urn:nbn:fi:lb-2017021609", "title": {"en": "Silva Kiuru's Time Expressions Corpus", "fi": "Silva Kiurun ajanilmausaineisto"}, "description": {"en": "This corpus of time expressions has been compiled from literary works, translations, dialect texts as well as other texts. Format: word documents.", "fi": "Tämä suomen kielen ajanilmauksia käsittävä aineisto on koottu kaunokirjallisten alkuperäisteosten, käännösten, murreaineistojen ja muiden tekstien pohjalta."},"modified": "2017-02-15T00:00:00.000000Z", "issued": "2017-02-15T00:00:00.000000Z", "access_rights": {"license": [{"url": "http://uri.suomi.fi/codelist/fairdata/license/code/undernegotiation"}], "access_type": {"url": "http://uri.suomi.fi/codelist/fairdata/access_type/code/open"}}}}
 
-def test_records_to_dict_with_last_harvest_date(single_record_response, create_test_log_file):
+def test_records_to_dict_with_last_harvest_date(single_record_to_dict, create_test_log_file):
     """
     Test that fetching records based on a date in log file succeeds (only updated records are fetched).
     """
     result = metadata_harvester_cli.records_to_dict("harvester_test.log")    
-    assert single_record_response == result
+    assert single_record_to_dict == result
 
-def test_records_to_dict_without_last_harvest_date(single_record_response):
+def test_records_to_dict_without_last_harvest_date(single_record_to_dict):
     """
     Test that fetching records without a log file succeeds (all records are fetched).
     """
     result = metadata_harvester_cli.records_to_dict("harvester_test.log")
-    assert single_record_response == result
+    assert single_record_to_dict == result
 
 @pytest.fixture
 def create_test_log_file():
@@ -91,3 +91,4 @@ def test_get_last_harvest_with_unsuccessful_harvests(create_test_log_file_with_u
     """Test getting the last start time of successful harvest date in the log file"""
     harvested_date =metadata_harvester_cli.last_harvest_date(create_test_log_file_with_unsuccessful_harvests)
     assert harvested_date == "2023-09-08T14:34:16Z"
+
