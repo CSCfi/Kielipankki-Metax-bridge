4abe7aef49e683f105e7d81c18065365d00cdba9
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-09-13 11:24:28 +0300

    KP-7440 Apply pylint formatting

--- harvester/pmh_interface.py
+++ harvester/pmh_interface.py
@@ -5,6 +5,7 @@ Fetch data from an OAI-PMH API of Metashare
 from sickle import Sickle
 from sickle.oaiexceptions import NoRecordsMatch
 
+
 class PMH_API:
     """
     Interface for fetching data from an OAI-PMH API
@@ -20,17 +21,19 @@ class PMH_API:
         """
         Fetch all non-deleted records from the API.
         """
-        metadata_records = self.sickle.ListRecords(metadataPrefix="info", ignore_deleted=True)
+        metadata_records = self.sickle.ListRecords(
+            metadataPrefix="info", ignore_deleted=True)
         for metadata_record in metadata_records:
             yield metadata_record
-    
+
     def fetch_changed_records(self, datetime):
         """
         Fetch records that are new or updated since a date.
         :param datetime: date (and time) string value
         """
         try:
-            metadata_records = self.sickle.ListRecords(**{"metadataPrefix": "info","from": datetime,"ignore_deleted":True})
+            metadata_records = self.sickle.ListRecords(
+                **{"metadataPrefix": "info", "from": datetime, "ignore_deleted": True})
             for metadata_record in metadata_records:
                 yield metadata_record
         except NoRecordsMatch:
