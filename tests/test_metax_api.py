c0ca7d8d462a969f3df16843d991fd0f16af3752
Kaisa Kuivalahti <kaisa.kuivalahti@csc.fi>
2023-09-26 16:28:34 +0300

    KP-7481 Fix DELETE tests and response checks

--- tests/test_metax_api.py
+++ tests/test_metax_api.py
@@ -68,7 +68,8 @@ def test_create_dataset_failed(mock_requests_post, caplog, metax_base_url, metax
     }
 
     with pytest.raises(requests.exceptions.RequestException), caplog.at_level(logging.ERROR):
-        metax_api.create_record(metadata_dict)
+        response = metax_api.create_record(metadata_dict)
+        assert response.status_code == 400
     assert mock_requests_post.request_history[0].method == "POST"
     assert mock_requests_post.call_count == 1
     assert "Request failed. Method: POST" in caplog.text
@@ -96,8 +97,8 @@ def test_update_dataset_successful(mock_requests_put, caplog, metax_api, mock_po
     }
 
     with caplog.at_level(logging.INFO):
-        response_text = metax_api.update_record(metax_dataset_id, sample_dict)
-    assert response_text == mock_post_put_response_json
+        response = metax_api.update_record(metax_dataset_id, sample_dict)
+    assert response == mock_post_put_response_json
     assert "Request succeeded. Method: PUT" in caplog.text
     assert mock_requests_put.call_count == 1
     assert mock_requests_put.request_history[0].method == "PUT"
@@ -128,27 +129,53 @@ def test_update_dataset_failed(mock_requests_put, caplog, metax_base_url, metax_
     }
 
     with pytest.raises(requests.exceptions.HTTPError), caplog.at_level(logging.ERROR):
-        metax_api.update_record(metax_dataset_id, sample_dict)
+        response = metax_api.update_record(metax_dataset_id, sample_dict)
+        assert response.status_code == 400
     assert "Request failed. Method: PUT" in caplog.text
     assert mock_requests_put.call_count == 1
     assert mock_requests_put.request_history[0].method == "PUT"
 
 
-# def test_delete_record(mock_delete_record, metax_dataset_id):
-#     """
-#     Test that a DELETE request of a dataset is successful (204 response).
-#     """
-#     response = metax_api.delete_dataset(metax_dataset_id)
-#     assert mock_delete_record.call_count == 1
-#     assert mock_delete_record.request_history[0].method == "DELETE"
-#     assert response.status_code == 204
+def test_delete_record(mock_delete_record, metax_dataset_id, metax_api, metax_base_url):
+    """
+    Test that a DELETE request of a dataset is successful (204 response).
+    """
+    mock_delete_record.delete(
+        f"{metax_base_url}/datasets/{metax_dataset_id}",
+        status_code=204,
+    )
+    response = metax_api.delete_record(metax_dataset_id)
+    assert mock_delete_record.call_count == 1
+    assert mock_delete_record.request_history[0].method == "DELETE"
+    assert response.status_code == 204
 
 
-# def test_datacatalog_dataset_record_pids(pids_list_in_datacatalog):
-#     """
-#     Test that querying a datacatalog in metax returns a list of all  its PIDs.
-#     """
-#     result = metax_api.datacatalog_dataset_record_pids()
-#     assert pids_list_in_datacatalog.request_history[0].method == "GET"
-#     assert pids_list_in_datacatalog.call_count == 1
-#     assert result == ["pid1", "pid2"]
+def test_delete_record_failed(
+    mock_delete_record,
+        metax_dataset_id,
+        metax_api,
+        caplog,
+        metax_base_url):
+    """
+    Test that a DELETE request of a dataset fails (404 response).
+    """
+    mock_delete_record.delete(
+        f"{metax_base_url}/datasets/{metax_dataset_id}",
+        status_code=404,
+    )
+    with pytest.raises(requests.exceptions.HTTPError), caplog.at_level(logging.ERROR):
+        response = metax_api.delete_record(metax_dataset_id)
+        assert response.status_code == 404
+    assert mock_delete_record.call_count == 1
+    assert mock_delete_record.request_history[0].method == "DELETE"
+    assert "Request failed. Method: DELETE" in caplog.text
+
+
+def test_datacatalog_dataset_record_pids(pids_list_in_datacatalog):
+    """
+    Test that querying a datacatalog in metax returns a list of all  its PIDs.
+    """
+    result = metax_api.datacatalog_dataset_record_pids()
+    assert pids_list_in_datacatalog.request_history[0].method == "GET"
+    assert pids_list_in_datacatalog.call_count == 1
+    assert result == ["pid1", "pid2"]
