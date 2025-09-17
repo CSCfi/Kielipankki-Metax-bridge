b5ecb9e358bc5c4054a0ca821012a9bb21fe448d
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2025-09-17 08:01:24 +0000

    KP-10352 Extract configuration reading to a reusable module
    To keep backwards compatibility, we don't want to mess with the
    metadata_harvester_cli too much (by e.g. adding new commands that would
    result in requiring specifying a command also for the normal harvest).
    Instead, let's prepare for adding another command line tool for doing
    manual one-off operations.

--- metadata_harvester_cli.py
+++ metadata_harvester_cli.py
@@ -14,8 +14,8 @@ from requests.exceptions import (
     HTTPError,
     RequestException,
 )
-import yaml
 
+from utils import cli_utils
 from harvester.metadata_parser import RecordParsingError
 from harvester.pmh_interface import PMH_API
 from metax_api import MetaxAPI
@@ -56,44 +56,6 @@ def last_harvest_date(log_file_path):
         return None
 
 
-def _config_from_file(config_file):
-    """
-    Read the YAML configuration from given file and return as a dict.
-
-    If the configuration file is malformed or missing some mandatory values, an
-    exception is raised.
-    """
-    try:
-        config = yaml.load(config_file, Loader=yaml.BaseLoader)
-    except yaml.YAMLError as e:
-        raise click.ClickException(
-            "Given configuration file does not seem to be in YAML fromat: "
-            f"{e}. See config/template.yml for valid configuration "
-            "file example."
-        )
-
-    if type(config) != dict:
-        raise click.ClickException(
-            "Unexpect configuration file structure. See config/template.yml for a "
-            "valid configuration file example."
-        )
-
-    expected_configuration_values = [
-        "metax_api_token",
-        "metax_base_url",
-        "metax_catalog_id",
-        "harvester_log_file",
-        "metax_api_log_file",
-    ]
-
-    for configuration_value in expected_configuration_values:
-        if configuration_value not in config:
-            raise click.ClickException(
-                f'Value for "{configuration_value}" not found in configuration file'
-            )
-    return config
-
-
 @click.command()
 @click.argument("config_file", type=click.File("r"), default="config/config.yml")
 @click.option(
@@ -114,7 +76,11 @@ def full_harvest(config_file, pause_between_records, automatic_delete):
 
     CONFIG_FILE Configuration for the harvesting. See config/template.yml for example.
     """
-    config = _config_from_file(config_file)
+    try:
+        config = cli_utils.config_from_file(config_file)
+    except cli_utils.ConfigurationError as err:
+        raise click.ClickException(str(err))
+
     source_api = PMH_API("https://clarino.uib.no/oai")
     destination_api = MetaxAPI(
         base_url=config["metax_base_url"],
