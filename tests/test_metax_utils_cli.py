3bd7ae4b21b61a5fbedf2018b71d7883f0a8af4e
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2025-09-19 20:17:26 +0000

    KP-10352 Add CLI tool for removing a record from Metax
    This is in a separate CLI module to keep the automatically run command
    backwards compatible and as idiot-proof as possible.

---
+++ tests/test_metax_utils_cli.py
@@ -0,0 +1,33 @@
+import logging
+
+from click.testing import CliRunner
+import pytest
+
+import metax_utils_cli
+
+
+@pytest.mark.usefixtures(
+    "mock_cmdi_record_found_in_datacatalog",
+    "mock_requests_get_record",
+)
+def test_delete_record(
+    metax_dataset_id,
+    mock_delete_record,
+    dataset_pid,
+    run_cli,
+):
+    """
+    Test deleting a single record from Metax successfully.
+    """
+    result = run_cli(metax_utils_cli.delete_record, extra_args=[dataset_pid])
+
+    assert result.exit_code == 0
+
+    assert mock_delete_record.call_count == 2  # GET and DELETE both shown here
+    assert mock_delete_record.request_history[0].method == "GET"
+    assert mock_delete_record.request_history[1].method == "DELETE"
+    assert metax_dataset_id in mock_delete_record.request_history[1].path
+
+    assert f"Deleting record {dataset_pid}" in result.stdout
+    assert f"{metax_dataset_id}" in result.stdout
+    assert "Record deleted" in result.stdout
